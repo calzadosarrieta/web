6fa8de870792593cfe532242fdd7337ab508e6cf
Calzados Arrieta <calzadosarrieta@gmail.com>
2024-04-04 10:52:10 +0200

    sandra

--- crearProductos.sh
+++ crearProductos.sh
@@ -46,8 +46,9 @@ order: $order
 
 }
 
-rm -r "$destination/*"
+
+rm -rf productos/*
 # Find all files within the specified subfolder and create YAML files
 find "$origin" -type f -print0 | while IFS= read -r -d '' file; do
-    create_yaml "$file"
+   create_yaml "$file"
 done
